5ba2049d21901bd6ee058df4ffec78e741e157d4
tomdaniels <thomsondaniels@hotmail.com>
2018-05-24 23:57:56 +1000

    import react on global.js

--- test/globals.js
+++ test/globals.js
@@ -1,12 +1,13 @@
 import chai from 'chai';
 import enzyme from 'enzyme';
+import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({
   adapter: new Adapter()
 });
 
+global.React = React;
 global.shallow = enzyme.shallow;
 global.expect = chai.expect;
 global.should = chai.should();
-global.React = require('react');
